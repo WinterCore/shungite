22527ac4496d05d5c0a8efd3e50a0d099253e9a1
WinterCore <hogobbl@gmail.com>
2021-01-30 20:20:44 +0200

    Replace single quoets with double quotes

--- chrome-extension/webpack.config.js
+++ chrome-extension/webpack.config.js
@@ -1,23 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
+    entry: "./src/index.ts",
+    mode: "development",
     module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-		],
+        rules: [{
+	    test: /\.tsx?$/,
+	    use: "ts-loader",
+	    exclude: /node_modules/,
+	}],
     },
-	devtool: 'source-map',
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-	},
-	output: {
-		filename: 'build.js',
-		path: path.resolve(__dirname, 'build'),
-	}
+    devtool: "source-map",
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "build.js",
+        path: path.resolve(__dirname, "build"),
+    }
 };
