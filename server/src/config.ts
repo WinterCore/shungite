eadfb4e4997226654af92cbcc6c85df2367cb935
WinterCore <hogobbl@gmail.com>
2021-01-31 23:33:44 +0200

    Add JWT_SECRET and JWT_EXPIRE_IN to config file

--- server/src/config.ts
+++ server/src/config.ts
@@ -9,3 +9,6 @@ const DB_CRED = DB_USER.length > 0 || DB_PASS.length > 0 ? `${DB_USER}:${DB_PASS
 export const MONGO_URL = process.env.DB_URL || `mongodb://${DB_CRED}${DB_HOST}:${DB_PORT}/${DB_NAME}${DB_CRED ? "?authSource=admin" : ""}`;
 
 export const PORT = process.env.PORT || 8081;
+
+export const JWT_SECRET = process.env.JWT_SECRET || "POTATO";
+export const JWT_EXPIRE_IN = process.env.JWT_EXPIRE_IN || 604800000; // 5 days
