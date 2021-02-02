dff4ffd542c787ce64d2802732612fdee6859bab
WinterCore <hogobbl@gmail.com>
2021-02-02 15:50:39 +0200

    Add cors support

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,18 +3,28 @@ import * as Morgan     from "morgan";
 import * as Helmet     from "helmet";
 import * as Path       from "path";
 import * as BodyParser from "body-parser";
+import * as Cors       from "cors";
 
 import ErrorHandler from "./errors/index";
 import Logger from "../logger";
 
 import API from "./api/index";
 
-import { PORT } from "../config";
+import { CORS_WHITELIST, PORT } from "../config";
 
 const app = Express();
 
-app.use(Helmet());
 app.set("trust proxy", 1);
+app.use(Helmet());
+app.use(Cors((req, callback) => {
+    const origin = req.header("Origin");
+    if (!origin || CORS_WHITELIST.indexOf(origin) > -1) {
+        callback(null, { origin: true });
+    } else {
+        callback(null, { origin: false });
+    }
+}));
+
 app.use(Morgan("tiny"));
 app.use(Express.static(Path.resolve("./public")));
 app.use(BodyParser.json());
