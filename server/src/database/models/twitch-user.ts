578dae7b864e88d578b92970d43eb18ccca07389
WinterCore <hogobbl@gmail.com>
2021-02-02 14:02:36 +0200

    Add methods to fetch public & private emotes

--- server/src/database/models/twitch-user.ts
+++ server/src/database/models/twitch-user.ts
@@ -1,17 +1,6 @@
 import * as Mongoose from "mongoose";
-
-const TwitchUserSchema: Mongoose.Schema = new Mongoose.Schema({
-    twitchId      : { type: String, unique: true, required: true },
-    username      : { type: String, unique: true, required: true },
-    name          : { type: String, required: true },
-    email         : { type: String, unique: true, required: true },
-    picture       : { type: String, required: true },
-    channelEmotes : {
-        type    : [{ type: Mongoose.Schema.Types.ObjectId, ref: "emote" }],
-        index   : true,
-        default : [],
-    },
-});
+import TwitchUserEmote from "./twitch-user-emote";
+import Emote, { EmoteDoc, EmoteStatus } from "./emote";
 
 export type TwitchUser = {
     twitchId      : string;
@@ -19,9 +8,29 @@ export type TwitchUser = {
     name          : string;
     email         : string;
     picture       : string;
-    channelEmotes : string[];
+    publicEmotes  : () => Promise<EmoteDoc[]>;
+    privateEmotes : () => Promise<EmoteDoc[]>;
 };
 
 export type TwitchUserDoc = TwitchUser & Mongoose.Document;
+
+const TwitchUserSchema: Mongoose.Schema<TwitchUserDoc> = new Mongoose.Schema({
+    twitchId  : { type: String, unique: true, required: true, index: true },
+    username  : { type: String, unique: true, required: true, index: true },
+    name      : { type: String, required: true },
+    email     : { type: String, unique: true, required: true },
+    picture   : { type: String, required: true },
+});
+
+TwitchUserSchema.methods.publicEmotes = async function publicEmotes(this: TwitchUserDoc): Promise<EmoteDoc[]> {
+    const publicEmotesIds: string[] = (await TwitchUserEmote.find({ user: this._id }, { emote: 1 }))
+        .map(doc => doc.emote.toString());
+
+    return Emote.find({ isPrivate: false, status: EmoteStatus.APPROVED, _id: { $in: publicEmotesIds } });
+};
+
+TwitchUserSchema.methods.privateEmotes = async function privateEmotes(this: TwitchUserDoc): Promise<EmoteDoc[]> {
+    return Emote.find({ status: EmoteStatus.APPROVED, owner: this._id })
+};
 
 export default Mongoose.model<TwitchUserDoc>("user", TwitchUserSchema);
